0bf7bd333825ab50d8d2bcf85e0a34e31b62881b
MohamedMoustakim <mohamedmoustakim77@gmail.com>
2025-05-29 14:20:17 +0100

    video

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,29 @@
+let BuModal = document.querySelector("#btn-1log")
+let modal = document.querySelector(".wrapper")
+let closeModal = document.querySelector("#fxmark")
+
+BuModal.addEventListener('click', () => {
+    modal.style.display = "flex"
+})
+closeModal.addEventListener('click',() => {
+    modal.style.display = "none"
+})
 
 
 
+let Video = document.querySelector("#video");
+let mydivmodal = document.querySelector("#vimodal");
+// let divPlayVideo = document.querySelector("#play-video")
+let evid = document.querySelector(".video-close");
 
+Video.addEventListener("click", () => {
+  mydivmodal.style.display = "block";
+});
 
+evid.addEventListener("click", () => {
+  mydivmodal.style.display = "none";
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// divPlayVideo.addEventListener("click", () => {
+//   mydivmodal.style.display = "block";
+// });
